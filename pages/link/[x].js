f4b87fd9c29782f71e11fc00dfaa9e48570e7528
Everest Oreizy <43426819+everestoreizy@users.noreply.github.com>
2021-01-13 12:11:03 -0800

    GIT COMMIT

--- pages/link/[x].js
+++ pages/link/[x].js
@@ -4,7 +4,7 @@ const Thing = () => {
     const router = useRouter()
     const { x } = router.query
 
-    return "<script>window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";</script>"
+    return "<script>window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';</script>"
 }
 
 export default Thing;
