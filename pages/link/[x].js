4b9abca8b383026a01d09ae45b9aadf1f716770b
Everest Oreizy <43426819+everestoreizy@users.noreply.github.com>
2021-01-13 12:17:43 -0800

    COMPONENTDIDMOUNT()>??

--- pages/link/[x].js
+++ pages/link/[x].js
@@ -1,10 +1,15 @@
-import { useRouter } from 'next/router'
+import { useRouter, Router } from 'next/router'
 
 const Thing = () => {
     const router = useRouter()
     const { x } = router.query
 
-    return <script>window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';</script>
+    return <title>x - shorterlinkify</title>
+}
+
+componentDidMount(){
+    const {pathname} = Router
+    Router.push('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 }
 
 export default Thing;
